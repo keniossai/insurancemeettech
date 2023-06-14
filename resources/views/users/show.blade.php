af3f47a7dc99a8b4472999260bb71c2aef19e113
keniossai <kenossai1@gmail.com>
2023-06-14 19:25:20 +0100

    Update show.blade.php

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -42,74 +42,100 @@
                     <form class="exvent-form x-submit text-center" enctype="multipart/form-data" data-then="reload" action="{{ route('api.users.photo.update', $user) }}">
                         @method('put')
 
-                        <label class="badge badge-info badge-pill" for="photo" style="cursor: pointer">
+                        <label class="badge bg-info badge-pill" for="photo" style="cursor: pointer">
                             <i class="fa fa-camera"></i> <small>Upload Photo</small> <x-spinner />
                         </label>
                         <input type="file" name="photo" id="photo" style="display: none" onchange="$(this).parents('form').submit()">
                     </form>
                     <h2 class="column-title text-center mt-3" style="font-size: 22px">
                         {{ $user->full_name }}
+                        <br>
                         <span>{{ $user->designation }}</span>
                         <span>{{ $user->organization }}</span>
                     </h2>
                     <p class="text-center">
                         {{ $user->email }} <x-verified :verified="$user->hasVerifiedEmail()" />
                     </p>
+                    <div class="col-md-12">
+                        @unlessadmin($user)
+                        <div class="text-center">
+                            <hr>
+
+                            @empty($payment)
+
+                                <p><label class="label label-warning p-2"><i class="fa fa-exclamation-triangle"></i> Registration Payment Pending </label></p>
+                                <p><a href="{{ route('users.payments.show', $user) }}" ><button style="padding: 10px 20px;" class="submit_btn">Pay now</button></a></p>
+
+                            @else
+                                <p>REGISTRATION NO: {{ $user->reg_number }}</p>
+
+                                <p>
+                                    <label class="label label-success p-2"><i class="fa fa-check-circle-o"></i> Registration Paid (NGN @money($payment->fee->amount))
+                                    </label>
+                                </p>
+                                <p>
+                                    {{ $payment->fee->name }} | {{ $payment->fee->attendance }} Attendance
+                                </p>
+                            @endif
+                        </div>
+                        @endadmin
+                    </div>
                 </div>
             </div>
+            
             <div class="col-lg-8">
                 <div class="hero-form mt-0 grey-bg">
-                    
+
                     <form class="exvent-form x-submit" action="{{ route('api.users.update', $user) }}" method="post"
                     data-then="alert">
                         <div class="row gy-4">
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
-                                    <input type="text" name="first_name" id="first_name" placeholder="First Name" required>
+                                    <input type="text" name="first_name" id="first_name" required value="{{ $user->first_name }}" :disabled="!can_update">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
-                                    <input type="text" name="last_name" id="last_name" placeholder="Last Name" required>
+                                    <input type="text" name="last_name" id="last_name" required value="{{ $user->middle_name }}" :disabled="!can_update">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
-                                    <input placeholder="e.g johndoe@example.com" name="email"
-                                    id="email" type="email" required>
+                                    <input type="text" name="last_name" id="last_name" required value="{{ $user->last_name }}" :disabled="!can_update">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
-                                    <input placeholder="e.g johndoe@example.com" name="email_confirmation"
-                                    id="email_confirmation" type="email" required>
+                                    <input name="email"
+                                    id="email" type="email" required value="{{ $user->email }}" :disabled="!can_update">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
                                     <input name="phone" id="phone" required maxlength="15" minlength="10"
-                                    type="tel" placeholder="e.g 07012345678" required>
+                                    type="tel" required value="{{ $user->phone }}" :disabled="!can_update">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
-                                    <input type="text" name="organisation" id="organisation" placeholder="Organisation" required>
+                                    <input type="text" name="organization" id="organization" required value="{{ $user->organization }}" :disabled="!can_update">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
-                                    <input type="text" name="designation" id="designation" placeholder="Designation" required>
+                                    <input type="text" name="designation" id="designation" required value="{{ $user->designation }}" :disabled="!can_update">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="single-input white-bg">
-                                    
-                                        <select class="single-input white-bg" name="gender" id="gender"
-                                            required>
-                                            <option value="">Select gender</option>
-                                            <option value="M">Male</option>
-                                            <option value="F">Female</option>
+                                        <select style="border: none; padding: 10px 25px; box-shadow: none; line-height: 30px; transition: all 0.3s linear;" class="form-control single-input white-bg" name="gender" id="gender"
+                                            required :disabled="!can_update">
+                                            <option value="{{ \App\Models\User::MALE }}">Male</option>
+                                            <option value="{{ \App\Models\User::FEMALE }}">Female</option>
                                         </select>
+                                        <script>
+                                            $('#gender option[value={{ $user->gender }}]').prop('selected', true);
+                                        </script>
                                     {{-- <input type="select" name="gender" id="gender" placeholder="gender" required> --}}
                                 </div>
                             </div>
@@ -135,7 +161,7 @@
                             </div>
                             <div class="col-md-4 text-center m-auto">
                                 <button type="submit" class="submit_btn">
-                                    Register <x-spinner />
+                                    Apply Changes  <x-spinner />
                                 </button>
                             </div>
 
