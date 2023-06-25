36303eee03d666b8c2d1c7a2f87ed2e1056f60b2
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-06-25 15:58:54 +0100

    Added create form

--- resources/views/admin/speaker/create.blade.php
+++ resources/views/admin/speaker/create.blade.php
@@ -2,118 +2,88 @@
 
 @section('content')
 
-<section class="content-main">
-    <div class="row">
-        <div class="col-9">
-            <div class="content-header">
-                <h2 class="content-title">Add New Speaker</h2>
-            </div>
-        </div>
-        <div class="col-md-9">
-            <div class="card mb-4">
-                <div class="card-header">
-                    <h4>Register a speaker</h4>
-                </div>
-                <div class="card-body">
-                    <form class="x-submit" action="{{ route('speaker.store') }}" method="post"
-                    data-then="alert" enctype="multipart/form-data">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <label class="form-label">Title</label>
-                                <select class="form-select" name="title" required id="title">
-                                    <option>Alh</option>
-                                    <option>Chief</option>
-                                    <option>Dr</option>
-                                    <option>Gov</option>
-                                    <option>Hon</option>
-                                    <option>HRH</option>
-                                    <option>Lady</option>
-                                    <option>Miss</option>
-                                    <option>Ms</option>
-                                    <option>Mrs</option>
-                                    <option>Mstr</option>
-                                    <option>Mr</option>
-                                    <option>Pres</option>
-                                    <option>Prof</option>
-                                    <option>Rev</option>
-                                    <option>SAN</option>
-                                    <option>Sen</option>
-                                    <option>Sir</option>
-                                    <option>Sis</option>
-                                </select>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="mb-4">
-                                    <label class="form-label">First Name</label>
-                                    <div class="row gx-2">
-                                        <input placeholder="" required id="first_name" name="first_name" type="text" class="form-control">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Add Speaker</h4>
+                        <form class="forms-sample">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <div class="form-group">
+                                        <label for="exampleSelectGender">Title</label>
+                                        <select class="form-control" name="title" id="title" required>
+                                            <option value="">Select title</option>
+                                            <option>Mr</option>
+                                            <option>Mrs</option>
+                                            <option>Prof</option>
+                                            <option>Eng</option>
+                                            <option>Dr</option>
+                                            <option>CEO</option>
+                                            <option>Co, Founder</option>
+                                            <option>Founder</option>
+                                            <option>Managing Director</option>
+                                            <option>Director</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">First Name</label>
+                                        <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">Last Name</label>
+                                        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">slug</label>
+                                        <input type="text" class="form-control" id="slug" name="slug" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputName1">Organization</label>
+                                        <input type="text" class="form-control" id="organization" name="organization" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleSelectGender">Gender</label>
+                                        <select class="form-control" id="gender" name="gender">
+                                            <option>Male</option>
+                                            <option>Female</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>File upload</label>
+                                        <input class="form-control file-upload-info" placeholder="Upload Image" id="photo" name="photo" type="file">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="exampleTextarea1">Biography</label>
+                                        <textarea class="form-control" id="bio" name="bio" rows="6"></textarea>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-4">
-                                <div class="mb-4">
-                                    <label class="form-label">Middle Name</label>
-                                    <input placeholder="" required id="middle_name" name="middle_name" type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <label class="form-label">Last Name</label>
-                                    <input placeholder="" required id="last_name" name="last_name" type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <label class="form-label">Designation</label>
-                                    <input placeholder="" required id="designation" name="designation" type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <label class="form-label">Organization</label>
-                                    <input placeholder="" required id="organization" name="organization" type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <label class="form-label">Slug</label>
-                                    <input placeholder="" required id="slug" name="slug" type="text" class="form-control">
-                                </div>
-                            </div>
 
-                        </div>
-                        <div class="mb-4">
-                            <label class="form-label">About(Biography)</label>
-                            <textarea placeholder="" required id="bio" name="bio" class="form-control" rows="4"></textarea>
-                        </div>
-                    </form>
-                </div>
-            </div> <!-- card end// -->
-        </div>
-        <div class="col-lg-3">
-            <div class="card mb-4">
-                <div class="card-header">
-                    <h4>Media</h4>
-                </div>
-                <div class="card-body">
-                    <div class="input-upload">
-                        <img src="{{ asset('imgs/theme/upload.svg') }}" alt="">
-                        <input class="form-control" name="photo" type="file">
+                            <button type="submit" class="btn btn-primary mr-2">Submit</button>
+                            <button class="btn btn-light">Cancel</button>
+                        </form>
                     </div>
                 </div>
-            </div> <!-- card end// -->
+            </div>
 
-        </div>
 
-        <div>
-            <button class="btn btn-light rounded font-sm mr-5 text-body hover-up">Cancel</button>
-            <button class="btn btn-md rounded font-sm" type="button">Publish <x-spinner /> </button>
         </div>
     </div>
-</section>
-<script>
-    form.addEventListener('submit', function(event){
-        event.preventDefault();
-    })
-</script>
 @endsection
